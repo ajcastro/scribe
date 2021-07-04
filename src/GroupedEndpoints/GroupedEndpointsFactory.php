06eaa63a67ed759287cb87dbb36376680300da33
ajcastro <ajcastro29@gmail.com>
2021-07-04 14:38:52 +0800

    Fix, return GroupedEndpointsFromCamelDir if no-extraction

--- src/GroupedEndpoints/GroupedEndpointsFactory.php
+++ src/GroupedEndpoints/GroupedEndpointsFactory.php
@@ -10,6 +10,10 @@ class GroupedEndpointsFactory
 {
     public static function make(GenerateDocumentation $command, RouteMatcherInterface $routeMatcher): GroupedEndpointsContract
     {
+        if ($command->option('no-extraction')) {
+            return new GroupedEndpointsFromCamelDir;
+        }
+
         if ($command->getDocConfig()->get('from_tests.enabled')) {
             return new GroupedEndpointsFromTests($command);
         }
@@ -21,7 +25,5 @@ class GroupedEndpointsFactory
         if ($command->shouldExtract()) {
             return new GroupedEndpointsFromApp($command, $routeMatcher, true);
         }
-
-        return new GroupedEndpointsFromCamelDir;
     }
 }
