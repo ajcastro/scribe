66aef609f26ce877f0c6f74c95a68076b2520514
ajcastro <ajcastro29@gmail.com>
2021-07-05 17:01:01 +0800

    Save

---
+++ src/Extracting/Strategies/BodyParameters/GetFromPerformedTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Knuckles\Scribe\Extracting\Strategies\BodyParameters;
+
+use Knuckles\Scribe\Extracting\Strategies\Strategy;
+
+class GetFromPerformedTest extends Strategy
+{
+    public function methodName()
+    {
+
+    }
+}
